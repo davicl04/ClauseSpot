2214a1f5338b46fa494b3ab2d6aa088e02adc814
Gabriel Teixeira <gabrielvinente144.gv@gmail.com>
2025-09-27 10:28:04 -0300

    feat: implementacao dashboard tela home

--- clausespot/src/app/home/page.tsx
+++ clausespot/src/app/home/page.tsx
@@ -1,10 +1,7 @@
 export default function HomePage() {
   return (
-    <div className="welcome-container">
-      <div className="welcome-card">
-        <h1>Bem-vindo ao <span>ClauseSpot</span>!</h1>
-        <p>Seu gerenciador inteligente de contratos.</p>
+      <div className="max-w-[2000px] max-h-[200] mx-auto">
+        <iframe width="100%" height="1000" src="https://lookerstudio.google.com/embed/reporting/bf8ab976-9296-4db3-af15-3298166579ea/page/YOiZF" sandbox="allow-storage-access-by-user-activation allow-scripts allow-same-origin allow-popups allow-popups-to-escape-sandbox"></iframe>
       </div>
-    </div>
   );
 }
